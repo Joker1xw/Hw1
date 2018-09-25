8b4354ffb6d26a08b7543e3d36ce71c47db0ed79
Alsahfiz <43392540+Alsahfiz@users.noreply.github.com>
2018-09-25 02:09:17 -0500

    Update MyDate.java

--- MyDate.java
+++ MyDate.java
@@ -1,42 +1,60 @@
-public class MyDate{
+public class MyDate {
+	
 	private int my_date_;
-
-	public MyDate(int date) {
+	private String my_date;
+	
+	public MyDate(String date) {
+		
+		String formatted = date.substring(6,10) + "" + date.substring(3,6) + "" + date.substring(0,3);
+		int integer_date = Integer.valueOf(formatted.replace("/", ""));
+		
+		SetMyDate(integer_date);
 		SetMyDate(date);
 	}
-
+	
 	public void SetMyDate(int date) {
 		//assume that the date will be in the format yyyymmdd
 		//date is an integer
 		my_date_ = date;
 	}
-
+	
 	public void SetMyDate(String date) {
 		//assume that the date will be in the format mm/dd/yyyy
-		//my_date_ =
+		my_date = date;
 	}
 
 	public int GetMyDate() {
 		return my_date_;
 	}
-
+	
 	public String GetMyDateString() {
-		System.out.prinln("Hello World");
-		//please return a date string in the format yyyymmdd
+		//please return a date string in the format yyyymmdd		
+		String formatted = my_date.substring(6,10) + ""
+	     + my_date.substring(3,6) + "" + my_date.substring(0,3);		
+		return formatted.replace("/", "");
 	}
-
+	
 	public String GetMyDateString1() {
-		//please return a date string in the format of mm/dd/yyyy
+		//please return a date string in the format of mm/dd/yyyy 
+		String formatted = my_date.substring(0,3)+ "" + my_date.substring(3,6) + "" + my_date.substring(6,10) ;
+		return formatted;
 	}
-
+	
 	public String GetMyDateString2() {
 		//please return a date string in the format of dd/mm/yyyy
+		String formatted = my_date.substring(3,6)+ "" + my_date.substring(0,3)+ "" + my_date.substring(6,10) ;
+		return formatted;
 	}
-
+	
 	public boolean IsLeapYear(int year) {
 		//please return true if the year is a leap year
 		//return false if the year is not a leap year
+        if (year % 400 == 0){
+        	return true;
+        }else if (year % 4 == 0) {
+        	return true;
+        }else {
+        	return false;
+        }
 	}
-
-
 }
