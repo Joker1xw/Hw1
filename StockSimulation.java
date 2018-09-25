47d47280fb0c866e280a0fb4947dc444ec6fe38a
Alsahfiz <43392540+Alsahfiz@users.noreply.github.com>
2018-09-25 02:11:28 -0500

    Update StockSimulation.java

--- StockSimulation.java
+++ StockSimulation.java
@@ -1,15 +1,23 @@
 public class StockSimulation{
 	public static void main(String[] args) {
-		MyDate date = new MyDate("01/01/2018");
+		MyDate date = new MyDate("02/01/2018");
 		
-		if(date.IsLeapYear(2018)) {
-			System.out.println("2018 is a leap year");
+		int year = 2018;
+		
+		if(date.IsLeapYear(year)) {
+			System.out.println(year + " is a leap year");
 		}else {
-			System.out.println("2018 is not a leap year");
+			System.out.println(year + " is not a leap year");
 		}
 		
 		Stock s = new Stock("Facebook", "FB", 178.33, date);
 		
 		//please check that every get method in the MyDate class is correct
+		
+		s.getMyDate();
+		s.getName();
+		s.getSymbol();
+		s.getCurrentPrice();
+		
 	}
 }
