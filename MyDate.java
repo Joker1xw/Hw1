07aed3c7531501d0f5de8bcaf5f0539e4f28dc65
Greg Rice <thatRiceFamily@yahoo.com>
2018-09-24 21:49:03 -0500

    Changes from Greg

--- MyDate.java
+++ MyDate.java
@@ -4,6 +4,10 @@ public class MyDate{
 	public MyDate(int date) {
 		SetMyDate(date);
 	}
+	
+	public MyDate(String date) {
+		SetMyDate(date);
+	}
 
 	public void SetMyDate(int date) {
 		//assume that the date will be in the format yyyymmdd
@@ -13,7 +17,11 @@ public class MyDate{
 
 	public void SetMyDate(String date) {
 		//assume that the date will be in the format mm/dd/yyyy
-		//my_date_ =
+		String month = date.substring(0, 2);
+		String day = date.substring(3, 5);
+		String year = date.substring(6, 10);
+		String date_ymd = year + month + day;
+		my_date_ = Integer.valueOf(date_ymd);
 	}
 
 	public int GetMyDate() {
@@ -21,21 +29,31 @@ public class MyDate{
 	}
 
 	public String GetMyDateString() {
-		System.out.prinln("Hello World");
 		//please return a date string in the format yyyymmdd
+		System.out.println("Hello World");
+		return String.valueOf(my_date_);
 	}
 
 	public String GetMyDateString1() {
 		//please return a date string in the format of mm/dd/yyyy
+		return "";
 	}
 
 	public String GetMyDateString2() {
 		//please return a date string in the format of dd/mm/yyyy
+		return "";
 	}
 
 	public boolean IsLeapYear(int year) {
 		//please return true if the year is a leap year
-		//return false if the year is not a leap year
+		//return false if the year is not a leap year		
+		
+		boolean isALeapYear;
+		
+	     // divisible by 4
+		isALeapYear = (year % 4 == 0);
+        
+        return isALeapYear;
 	}
 
 
